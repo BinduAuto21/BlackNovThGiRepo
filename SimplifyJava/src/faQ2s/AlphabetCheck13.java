11d889252a78a38ab3155d296167229d208a8e88
Bindu Karnam <bkarnam74@gmail.com>
2023-11-13 15:46:24 -0500

    after removing the extra dot git hidden inside the project folder-my commit is working

---
+++ SimplifyJava/src/faQ2s/AlphabetCheck13.java
@@ -0,0 +1,17 @@
+package faQ2s;
+
+public class AlphabetCheck13 {
+
+	public static void main(String[] args) {
+		char c='a';
+		
+		if((c>='a'&& c<='z')||(c>='A'&& c<='Z')) {
+			System.out.println(char.class+" is an alphabet");
+		}else
+		{
+			System.out.println(char.class+" is not an alphabet");
+		}
+
+	}
+
+}
